98bb783ee514d2c0ae884e1fe94f35539a7ddc3f
Tish Chungoora <tishal82@gmail.com>
2019-12-03 11:13:48 +0000

    Created index methods in the Sounds and Compositions controller; also included user data and sound data in the Compositions controller

--- chillax-backend/app/controllers/compositions_controller.rb
+++ chillax-backend/app/controllers/compositions_controller.rb
@@ -1,2 +1,6 @@
 class CompositionsController < ApplicationController
+    def index
+        compositions  = Composition.all
+        render json: compositions, include: [:sounds, :user]
+    end
 end
