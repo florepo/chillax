d830e9e03be2c13489b4ebd5a1ae27eeea889f4c
flow1981 <contact.f.stagliano@gmail.com>
2019-12-04 09:55:36 +0000

    moved git ignore file to backendfolder

--- chillax-backend/app/controllers/compositions_controller.rb
+++ chillax-backend/app/controllers/compositions_controller.rb
@@ -1,6 +1,6 @@
 class CompositionsController < ApplicationController
     def index
-        compositions  = Composition.all
+        compositions = Composition.all
         render json: compositions, include: [:sounds, :user]
     end
 end
