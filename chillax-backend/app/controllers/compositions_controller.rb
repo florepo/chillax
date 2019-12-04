4d0b14b3c6acc18441bf597bb28366b453775c7d
flow1981 <contact.f.stagliano@gmail.com>
2019-12-04 11:18:15 +0000

    implemented save compostion fucntionality

--- chillax-backend/app/controllers/compositions_controller.rb
+++ chillax-backend/app/controllers/compositions_controller.rb
@@ -3,4 +3,28 @@ class CompositionsController < ApplicationController
         compositions = Composition.all
         render json: compositions, include: [:sounds, :user]
     end
+
+    def create
+      
+
+      name = params[params.keys[-5]]
+      user_id = params[params.keys[-4]]
+
+      composition = Composition.create(name: name, user_id: user_id)
+
+       # identify the number pof compositon sounds for this compsition
+       number_of_sounds = params.keys[0..-6].length
+       
+       # => loop over all sounds
+       number_of_sounds.times do |index|
+          sound_id = params[params.keys[index]]["id"]
+          volume = params[params.keys[index]]["volume"]
+          CompositionSound.create(sound_id: sound_id, volume: volume, composition_id: composition.id)
+          #byebug
+       end
+      
+
+
+
+    end
 end
